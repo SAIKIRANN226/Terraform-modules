83bf87a3ad341418ad6417751df9983700f9fb11
mothukuru saikiran <obscuresai.com>
2024-07-24 19:39:19 +0530

    message

---
+++ roboshop-ec2/ec2.tf
@@ -0,0 +1,5 @@
+module "roboshop_ec2" {
+  source = "../ec2"    # comming one step back and using ec2 source code,provider.tf will be in the user project not in the ec2 module
+  instance_type = var.instance_typeeeeee
+  tags = var.tagssssss
+}
