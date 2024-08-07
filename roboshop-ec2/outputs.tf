65836191e6d938347de2da69db3371bd9339b448
mothukuru saikiran <obscuresai.com>
2024-08-07 15:08:30 +0530

    message

--- roboshop-ec2/outputs.tf
+++ roboshop-ec2/outputs.tf
@@ -1,5 +1,5 @@
-output "public" {
-  value = module.roboshop_ec2.public_ip
+output "public" {            # here you can give as per your wish
+  value = module.roboshop_ec2.public_ip  # here you need to refer module only,that means use the correct names
 }
 
 output "private" {
