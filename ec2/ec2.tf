5647b6bc626d1d36aa0d7786dea6f3aa82b1cce6
mothukuru saikiran <obscuresai.com>
2024-09-03 15:17:40 +0530

    message

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,3 +3,9 @@ resource "aws_instance" "module" {
   instance_type = var.instance_type
   tags = var.tags
 }
+
+
+# ec2 folder ---> ec2 module
+# ec2.tf ---> resource definition
+# variables.tf ---> variables declaration
+# no provider in the module developing only should be in the roboshop-ec2
